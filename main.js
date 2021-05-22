ba0364396372c2764c3a8b76c4a400fc9f83712e
JA+ <601286825@qq.com>
2021-05-22 21:16:44 +0800

    try to support electron

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+let win = null;
+app.whenReady().then(() => {
+  Menu.setApplicationMenu(null); // 取消菜单栏
+  win = new BrowserWindow({
+    height: 400,
+    width: 400,
+    minWidth: 400,
+    minHeight: 400,
+    center: true,
+    // frame: false,
+    // resizable: false,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+  win.loadFile('index.html');
+  win.on('closed', function () {
+    win = null;
+  })
+})
+app.on('window-all-closed', function () {
+  app.quit();
+})
